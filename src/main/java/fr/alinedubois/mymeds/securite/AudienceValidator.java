36e9ffc84745d3fcb55f9da8942a468643f7b236
Aline Dubois <juillet.aline@gmail.com>
2022-05-07 12:42:44 +0200

    Ajout de traces pour Heroku

--- src/main/java/fr/alinedubois/mymeds/securite/AudienceValidator.java
+++ src/main/java/fr/alinedubois/mymeds/securite/AudienceValidator.java
@@ -1,11 +1,15 @@
 package fr.alinedubois.mymeds.securite;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
 import org.springframework.security.oauth2.jwt.Jwt;
 
 class AudienceValidator implements OAuth2TokenValidator<Jwt> {
+    private static final Logger logger = LoggerFactory.getLogger(AudienceValidator.class);
+
     private final String audience;
 
     AudienceValidator(String audience) {
@@ -14,7 +18,8 @@ class AudienceValidator implements OAuth2TokenValidator<Jwt> {
 
     public OAuth2TokenValidatorResult validate(Jwt jwt) {
         OAuth2Error error = new OAuth2Error("invalid_token", "The required audience is missing", null);
-        
+        logger.debug(jwt.getClaims().toString());
+        logger.debug("audience = " + audience);
         if (jwt.getAudience().contains(audience)) {
             return OAuth2TokenValidatorResult.success();
         }
