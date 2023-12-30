da614501144eb48934f4d63c07d101c66cf35a63
Aline Dubois <juillet.aline@gmail.com>
2023-12-30 19:52:53 +0100

    Déploiement de l'application

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-docker build -t alinedubois/mymeds:latest .
 docker rm -v -f postgres | true
 docker rm -v -f mymeds | true
 docker run -d --name postgres -v ~/postgresql-data:/var/lib/postgresql/data -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=password -e POSTGRES_DB=postgres -p 5432:5432 postgres:alpine
-docker run -d --name mymeds -e SPRING_DATASOURCE_URL=jdbc:postgresql://192.168.1.45:5432/postgres -p 8080:8080 alinedubois/mymeds:latest
+docker run -d --name mymeds -e SPRING_DATASOURCE_URL=jdbc:postgresql://192.168.1.44:5432/postgres -p 8080:8080 alinedubois/mymeds:latest
