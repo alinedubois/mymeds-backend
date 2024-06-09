8c14a83fc997a071cbe7b891142cb60e087fc3e6
Aline Dubois <juillet.aline@gmail.com>
2024-06-09 09:49:42 +0200

    Déploiement avec HTTPS

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ docker rm -v -f mymeds | true
 docker rm -v -f https | true
 docker run -d --name postgres -v ~/postgresql-data:/var/lib/postgresql/data -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=password -e POSTGRES_DB=postgres -p 5432:5432 postgres:alpine
 docker run -d --name mymeds -e SPRING_DATASOURCE_URL=jdbc:postgresql://192.168.1.110:5432/postgres -p 8081:8081 alinedubois/mymeds:latest
-docker run -d --name https --net=host -p 80:80 -p 443:443 -v ./Caddyfile:/etc/caddy/Caddyfile -v ./caddy_data:/data -v ./caddy_config:/config caddy:latest
+docker run -d --name https --net=host -p 80:80 -p 443:443 -v $(pwd)/Caddyfile:/etc/caddy/Caddyfile -v $(pwd)/../caddy_data:/data -v $(pwd)/../caddy_config:/config caddy:latest
